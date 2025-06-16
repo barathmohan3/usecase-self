256c589db1354223ef988fe5f9eb970a69a9efe3
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-16 21:54:38 +0530

    Update main.tf

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -34,7 +34,7 @@ resource "random_id" "lambda_suffix" {
 resource "aws_lambda_function" "expense_lambda" {
   function_name    = "expenseTracker_${random_id.lambda_suffix.hex}"
   runtime          = "python3.11"
-  handler          = "lambda_function.handler"
+  handler          = "lambda_function.lambda_handler"
   filename         = data.archive_file.lambda_zip.output_path
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   role             = aws_iam_role.lambda_exec.arn
