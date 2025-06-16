7b192c3d38d469ad5fa7eee6b5bd66a1eb459bbd
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-16 16:44:58 +0530

    Update main.tf

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -9,7 +9,7 @@ data "archive_file" "lambda_zip" {
 }
 
 resource "aws_iam_role" "lambda_exec" {
-  name = "lambda_exec_role"
+  name = "lambda_role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
