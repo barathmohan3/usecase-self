5308c86747d40564fd3239e9c5586b20234be8a8
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-16 23:14:47 +0530

    Update main.tf

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -22,6 +22,11 @@ resource "aws_iam_role" "lambda_exec" {
   })
 }
 
+resource "aws_cloudwatch_log_group" "lambda_logs" {
+  name              = "/aws/lambda/${aws_lambda_function.expense_lambda.function_name}"
+  retention_in_days = 7
+}
+
 resource "aws_iam_role_policy_attachment" "lambda_logs" {
   role       = aws_iam_role.lambda_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
