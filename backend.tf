eea575c6b3f6254e4f6db716cab1440b98642164
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-16 17:00:06 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "my-oidc-bucket-15328069840"
-    key            = "self/terraform.tfstate"
+    key            = "logs/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
   }
