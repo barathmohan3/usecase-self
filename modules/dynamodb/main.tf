a509fa5b7490a2c295849af7a96221d2a9de44f7
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-16 17:04:57 +0530

    Update main.tf

--- modules/dynamodb/main.tf
+++ modules/dynamodb/main.tf
@@ -1,5 +1,9 @@
+resource "random_id" "dynamodb_suffix" {
+  byte_length = 4
+}
+
 resource "aws_dynamodb_table" "expenses" {
-  name           = "Expenses"
+  name           = "Expenses_${random_id.dynamodb_suffix.hex}"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "userId"
   range_key      = "date"
