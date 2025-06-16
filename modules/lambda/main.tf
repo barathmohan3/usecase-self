c263b9f4dd64012f10d322bee817fc5616657cae
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-16 16:33:16 +0530

    Update main.tf

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -1,3 +1,13 @@
+provider "aws" {
+  region = var.region
+}
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "${path.module}/lambda_function.py"
+  output_path = "${path.module}/lambda_function.zip"
+}
+
 resource "aws_iam_role" "lambda_exec" {
   name = "lambda_exec_role"
   assume_role_policy = jsonencode({
@@ -18,14 +28,13 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
 }
 
 resource "aws_lambda_function" "expense_lambda" {
-  function_name = "expenseTracker"
-  runtime       = "python3.11"
-  handler       = "lambda_function.lambda_handler"
-  filename      = "${path.module}/lambda_function.py"     # <--- Direct file reference
-  source_code_hash = filebase64sha256("${path.module}/lambda_function.py")
-  role = aws_iam_role.lambda_exec.arn
+  function_name    = "expenseTracker"
+  runtime          = "python3.11"
+  handler          = "lambda_function.lambda_handler"
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  role             = aws_iam_role.lambda_exec.arn
 }
- 
 
 output "lambda_invoke_arn" {
   value = aws_lambda_function.expense_lambda.invoke_arn
